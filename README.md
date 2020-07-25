6b085cff5d8bc0b6d39ca3dbb5ab31b4aba3a431
aninternettroll <lucafulger@gmail.com>
2020-07-25 19:36:41 +0200

    Changed to using cogs

--- README.md
+++ README.md
@@ -14,9 +14,10 @@ For the +wrcount and +runcount commands, *Mobile* can be used as a **platform** 
 Also, in place of the parameter **user**, users can place an array containing a list of names (separated by commas) for which the commands will be executed. For example, instead of typing **+wrs user1 iOS** then **+wrs user2 iOS** then **+wrs user3 iOS**, users can type **+wrs [user1,user2,user3] iOS** to get the same output in one command. Note that this sends the result to the person executing the command as a direct message to reduce spam.
 
 ## Usage (hosts/developers)
-The main file for the bot is "Speedrun Bot.py", the other .py files containing functions that the main file imports.
+The main file for the bot is "bot.py", the other .py files containing functions that the main file imports.
+Commands should be put in cogs/src.py as it will make it easier to update the bot.
 
-Before running the bot, create a text file in the same directory called "token.txt". Its content must be the ID token for the bot, and nothing else. It is also recommended to change the text contained in the +invite command.
+Before running the bot, create a text file in the same directory called "token.txt". Its content must be the ID token for the bot, and nothing else. 
 
 ## Usage Permissions
 Features from this bot may be added to other bots as long as this page (https://github.com/Slush0Puppy/speedrunbot) is credited somewhere such as in documentation or in a command.
---
+++ cogs/admin.py
@@ -0,0 +1,72 @@
+import os
+
+import git
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.is_owner()
+    @commands.command(name='reload', usage='<extension>')
+    async def _reload(self, ctx, ext):
+        """Reloads an extension"""
+        try:
+            self.bot.reload_extension(f'cogs.{ext}')
+            await ctx.send(f'The extension {ext} was reloaded!')
+        except commands.ExtensionNotFound:
+            await ctx.send(f'The extension {ext} doesn\'t exist.')
+        except commands.ExtensionNotLoaded:
+            await ctx.send(f'The extension {ext} is not loaded! (use {ctx.prefix}load)')
+        except commands.NoEntryPointError:
+            await ctx.send(f'The extension {ext} doesn\'t have an entry point (try adding the setup function) ')
+        except commands.ExtensionFailed:
+            await ctx.send(f'Some unknown error happened while trying to reload extension {ext} (check logs)')
+            self.bot.logger.exception(f'Failed to reload extension {ext}:')
+
+    @commands.is_owner()
+    @commands.command(name='load', usage='<extension>')
+    async def _load(self, ctx, ext):
+        """Loads an extension"""
+        try:
+            self.bot.load_extension(f'cogs.{ext}')
+            await ctx.send(f'The extension {ext} was loaded!')
+        except commands.ExtensionNotFound:
+            await ctx.send(f'The extension {ext} doesn\'t exist!')
+        except commands.ExtensionAlreadyLoaded:
+            await ctx.send(f'The extension {ext} is already loaded.')
+        except commands.NoEntryPointError:
+            await ctx.send(f'The extension {ext} doesn\'t have an entry point (try adding the setup function)')
+        except commands.ExtensionFailed:
+            await ctx.send(f'Some unknown error happened while trying to reload extension {ext} (check logs)')
+            self.bot.logger.exception(f'Failed to reload extension {ext}:')
+
+    @commands.is_owner()
+    @commands.command(name='unload', usage='<extension>')
+    async def _unload(self, ctx, ext):
+        """Loads an extension"""
+        try:
+            self.bot.unload_extension(f'cogs.{ext}')
+            await ctx.send(f'The extension {ext} was unloaded!')
+        except commands.ExtensionNotFound:
+            await ctx.send(f'The extension {ext} doesn\'t exist!')
+        except commands.NoEntryPointError:
+            await ctx.send(f'The extension {ext} doesn\'t have an entry point (try adding the setup function)')
+        except commands.ExtensionFailed:
+            await ctx.send(f'Some unknown error happened while trying to reload extension {ext} (check logs)')
+            self.bot.logger.exception(f'Failed to unload extension {ext}:')
+
+    @commands.is_owner()
+    @commands.command(name="pull")
+    async def pull(self, ctx):
+        """Update the bot from github"""
+        g = git.cmd.Git(os.getcwd())
+        try:
+            await ctx.send(f"Probably pulled.\n```\n{g.pull()}```")
+        except git.exc.GitCommandError as e:
+            await ctx.send(f"An error has occured when pulling```\n{e}```")
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
